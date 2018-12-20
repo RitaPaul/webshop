d22b234e9d4fc87d9ceb07ca9218cf7d4825fd50
RitaPaul <ritapaul1@hotmail.com>
2018-12-20 01:00:20 +0100

    Initial commit

---
+++ index.jsx
@@ -0,0 +1,629 @@
+	var ITEMS = [
+	{
+		id: 0,
+		path:"./pics/headphones.jpg",
+		name: "Apple HeadPhones",
+		price: 200,
+		numberOfItems: 0,
+
+	},
+	{
+		id: 1,
+		path:"./pics/camera.jpg",
+		name: "High-Quality Canon",
+		price: 5000,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 2,
+		path:"./pics/iMac.jpg",
+		name: "IMAC 27 in",
+		price: 13449,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 3,
+		path:"./pics/macbook.jpg",
+		name: "MacBook Pro i7",
+		price: 9799,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 4,
+		path:"./pics/iphone.jpg",
+		name: "iPhone XS",
+		price: 10239,
+		numberOfItems: 0,
+
+	},
+	{
+		id: 5,
+		path:"./pics/keyboard.jpg",
+		name: "Magic Keyboard with Numeric Keypad",
+		price: 999,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 6,
+		path:"./pics/mouse.jpg",
+		name: "Magic Mouse",
+		price: 585,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 7,
+		path:"./pics/smart-watch.jpg",
+		name: "Dieselon Full Guard",
+		price: 4500,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 8,
+		path:"./pics/handfree.jpg",
+		name: "Apple AirPods",
+		price: 99,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 9,
+		path:"./pics/headphones.jpg",
+		name: "Apple HeadPhones",
+		price: 200,
+		numberOfItems: 0,
+
+	},
+	{
+		id: 10,
+		path:"./pics/camera.jpg",
+		name: "High-Quality Canon",
+		price: 5000,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 11,
+		path:"./pics/iMac.jpg",
+		name: "IMAC 27 in",
+		price: 13449,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 12,
+		path:"./pics/macbook.jpg",
+		name: "MacBook Pro i7",
+		price: 9799,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 13,
+		path:"./pics/iphone.jpg",
+		name: "iPhone XS",
+		price: 10239,
+		numberOfItems: 0,
+
+	},
+	{
+		id: 14,
+		path:"./pics/keyboard.jpg",
+		name: "Magic Keyboard with Numeric Keypad",
+		price: 999,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 15,
+		path:"./pics/mouse.jpg",
+		name: "Magic Mouse",
+		price: 585,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 16,
+		path:"./pics/smart-watch.jpg",
+		name: "Dieselon Full Guard",
+		price: 4500,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 17,
+		path:"./pics/handfree.jpg",
+		name: "Apple AirPods",
+		price: 99,
+		numberOfItems: 0,
+
+
+	},
+
+	{
+		id: 18,
+		path:"./pics/camera.jpg",
+		name: "High-Quality Canon",
+		price: 5000,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 19,
+		path:"./pics/iMac.jpg",
+		name: "IMAC 27 in",
+		price: 13449,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 20,
+		path:"./pics/macbook.jpg",
+		name: "MacBook Pro i7",
+		price: 9799,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 21,
+		path:"./pics/iphone.jpg",
+		name: "iPhone XS",
+		price: 10239,
+		numberOfItems: 0,
+
+	},
+	{
+		id: 22,
+		path:"./pics/keyboard.jpg",
+		name: "Magic Keyboard with Numeric Keypad",
+		price: 999,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 23,
+		path:"./pics/mouse.jpg",
+		name: "Magic Mouse",
+		price: 585,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 24,
+		path:"./pics/smart-watch.jpg",
+		name: "Dieselon Full Guard",
+		price: 4500,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 25,
+		path:"./pics/handfree.jpg",
+		name: "Apple AirPods",
+		price: 99,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 26,
+		path:"./pics/mouse-on-mat.jpg",
+		name: "Apple Mouse-Pad",
+		price: 150,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 27,
+		path:"./pics/bluetooth-speaker.jpg",
+		name: "New Sony Bluetooth-Speakers",
+		price: 1299,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 28,
+		path:"./pics/lamp-1.jpg",
+		name: "BMB-Lights for Living Room",
+		price: 1399,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 29,
+		path:"./pics/lamp-2.jpg",
+		name: "Electrolux-Lights for Garden",
+		price: 1599,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 30,
+		path:"./pics/lamp-4.jpg",
+		name: "Unique-Kitchen Lights",
+		price: 649,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 31,
+		path:"./pics/cycle.jpg",
+		name: "New Racer Bike",
+		price: 9000,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 32,
+		path:"./pics/lipstick-all-colors.jpg",
+		name: "Maybelline Lipsticks",
+		price: 699,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 33,
+		path:"./pics/lipstick-red-mat.jpg",
+		name: "Red-Hot Mat",
+		price: 199,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 34,
+		path:"./pics/lipstick-red.jpg",
+		name: "MAX-FactorRed-Shiney-Lips",
+		price: 99,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 35,
+		path:"./pics/lipstick-purple.jpg",
+		name: "Lovely-Purple by assence",
+		price: 29,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 36,
+		path:"./pics/lipstick-pink.jpg",
+		name: "Lovely-Pink by assence",
+		price: 4500,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 37,
+		path:"./pics/lipstick-ginger-red.jpg",
+		name: "Ginger-Red Mat by Lakme",
+		price: 112,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 37,
+		path:"./pics/lipstick-no-color.jpg",
+		name: "Natural Lips",
+		price: 300,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 38,
+		path:"./pics/lipstick-ginger-red.jpg",
+		name: "Ginger-Red Mat by Lakme",
+		price: 112,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 39,
+		path:"./pics/lipstick-red-mat.jpg",
+		name: "Red-Hot Mat",
+		price: 199,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 40,
+		path:"./pics/lipstick-red.jpg",
+		name: "MAX-FactorRed-Shiney-Lips",
+		price: 99,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 41,
+		path:"./pics/lipstick-purple.jpg",
+		name: "Lovely-Purple by assence",
+		price: 29,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 42,
+		path:"./pics/lipstick-pink.jpg",
+		name: "Lovely-Pink by assence",
+		price: 4500,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 43,
+		path:"./pics/lipstick-ginger-red.jpg",
+		name: "Ginger-Red Mat by Lakme",
+		price: 112,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 44,
+		path:"./pics/lipstick-no-color.jpg",
+		name: "Natural Lips",
+		price: 300,
+		numberOfItems: 0,
+
+
+	},
+	{
+		id: 45,
+		path:"./pics/lipstick-ginger-red.jpg",
+		name: "Ginger-Red Mat by Lakme",
+		price: 112,
+		numberOfItems: 0,
+
+
+	},
+	];
+
+	
+	var subTotal =0;
+	let counter = 0;
+	let totalItems = 0;
+
+	function Total(props){
+		return(
+			<div className = "total color">
+			<p className="color">Total Items: {props.totalItems} </p>
+			<p className="color"> Grand Total: {props.totalAmount} </p>
+			</div>
+			);
+	}
+
+	Total.propTypes = {
+		totalItems:React.PropTypes.number.isRequired,
+		totalAmount:React.PropTypes.number.isRequired,
+	};
+
+	function AddNewItem(props){
+		return(
+			<div className="hover-me">
+			<div className = "remove-item font-weight-bold"  onClick={function(){props.removeOnClick();}}>x</div>
+			<div className=" d-flex flex-wrap added-item">
+			<img src={props.path} className="img-icon"/> 
+			<div className=" d-flex flex-wrap flex-column editItems">
+			<p className="font-size-small color">{props.name} </p>
+			<p className="font-size-small color">{props.price} DKK</p>
+			<ActionButtons onChange={props.onItemChange}  numberOfItems={props.numberOfItems} />
+			</div>
+			</div>
+			</div>
+			);
+	}
+
+	AddNewItem.propTypes = {
+		numberOfItems:React.PropTypes.number,
+		path:React.PropTypes.string.isRequired,
+		name:React.PropTypes.string.isRequired,
+		price:React.PropTypes.number.isRequired,
+		onItemChange: React.PropTypes.func.isRequired,
+		removeOnClick:React.PropTypes.func,
+	};
+
+	function ActionButtons(props){
+		return(
+			<div className="d-flex flex-wrap">
+			<a className="actionButton add" onClick={function(){props.onChange(1);}}>+</a>
+			<div className="numberOfItems">{props.numberOfItems}</div>
+			<a className="actionButton remove" onClick={function(){props.onChange(-1);}}>-</a>
+			</div>
+			);
+	}
+	ActionButtons.propTypes = {
+		onChange: React.PropTypes.func.isRequired,
+		numberOfItems:React.PropTypes.number.isRequired,
+	};
+
+	function AddButton(props){
+		return(
+			<div>
+			<a className="actionButton add addButton" onClick={function(){props.onChange(1);}}>+</a>
+			</div>
+			);
+	}
+	ActionButtons.propTypes = {
+		onChange: React.PropTypes.func.isRequired,
+	};
+
+	function Item(props){
+		return(
+			<div classNeme="">
+			<img src={props.image_url} className="img-thumbnail" /> 
+			<div className="text-secondary d-flex flex-wrap justify-content-center align-items-end font-size-small">{props.name}</div>
+			<div  className="font-size-xs font-weight item-price">DKK: {props.price}</div>
+			</div>
+			);
+	}
+	Item.propTypes = {
+		image_url:React.PropTypes.string.isRequired,
+		name:React.PropTypes.string.isRequired,
+		price:React.PropTypes.number.isRequired,
+	};
+
+	function AddContent(props){
+		return(
+			<div >
+			<Item image_url={props.path} name={props.item} price={props.price} />
+			<div className=" d-flex flex-wrap  justify-content-end">
+			<AddButton onChange={props.onItemChange} />
+			</div>
+			</div>
+			);
+	}
+	AddContent.propTypes = {
+		path:React.PropTypes.string.isRequired,
+		item:React.PropTypes.string.isRequired,
+		price:React.PropTypes.number.isRequired,
+		onItemChange: React.PropTypes.func.isRequired,	
+		numberOfItems: React.PropTypes.number.isRequired,
+	};
+
+
+	var Application = React.createClass({
+
+		propTypes:{
+			initialItems: React.PropTypes.arrayOf(React.PropTypes.shape({
+				id: React.PropTypes.number.isRequired,
+				path: React.PropTypes.string.isRequired,
+				name: React.PropTypes.string.isRequired,
+				price: React.PropTypes.number.isRequired,
+				numberOfItems: React.PropTypes.number.isRequired,
+			})).isRequired,
+		},
+		getInitialState: function(){
+			return{
+				items: this.props.initialItems,
+				itemsInCart: 0,
+				grandTotal: 0,
+				priceOfItem: 0,
+			};
+		},
+		onItemChange: function(index , delta)
+		{
+			subTotal = this.state.items[index].price ;
+			if(delta == 1){
+				grandTotal: this.state.grandTotal += subTotal;
+				counter = this.state.items[index].numberOfItems += delta
+				totalItems = subTotal *= this.state.items[index].numberOfItems; 
+				itemsInCart: this.state.itemsInCart += delta;
+			}
+			else if(delta == -1){
+				grandTotal: this.state.grandTotal -= subTotal;
+				this.state.items[index].numberOfItems += delta;
+				itemsInCart: this.state.itemsInCart += delta;
+			}
+			this.setState(this.state);
+		},
+
+		onItemRemove:function(index){
+
+			var temp =  this.state.items[index].price*this.state.items[index].numberOfItems;
+			var temp2 = this.state.items[index].numberOfItems;
+			grandTotal:this.state.grandTotal -= temp;
+			itemsInCart:this.state.itemsInCart-=temp2;
+			this.state.items[index].numberOfItems=0;
+			this.setState(this.state);
+		},
+		render: function(){
+			return(
+				<div>
+				<nav className=" navbar navbar-details fixed-top justify-content-between">
+				<a className="navbar-brand" href="#">Buy with Me</a>
+				<div className="dropdown">
+				<button className="dropbtn clearfix" ><img src="./pics/cart.png" className="cart float-left"/> <p className="items-in-cart float-left">{this.state.itemsInCart}</p><p className="pay float-right">Pay: {this.state.grandTotal}</p></button> 
+				<div className="dropdown-content" >
+				{this.state.items.map(function(item,index){
+					if(item.numberOfItems > 0){
+						return(
+							<div>
+							<AddNewItem 
+							onItemChange={function(delta) {this.onItemChange(index, delta)}.bind(this)}
+							removeOnClick={function() {this.onItemRemove(index)}.bind(this)}
+							path={item.path}
+							name={item.name} 
+							price={item.price}
+							numberOfItems = {item.numberOfItems}
+							key={item.id}
+							/> 
+							</div>
+							)
+					}
+				}.bind(this))
+			}
+			<Total totalItems = {this.state.itemsInCart} totalAmount={this.state.grandTotal} />
+			</div>
+			</div>
+			</nav>
+			<div className="backgound-1"></div>
+			<div className="container d-flex flex-wrap justify-content-center ">
+			{this.state.items.map(function(item, index)
+				{
+					return(
+						<div className="item">
+						<AddContent
+						onItemChange={function(delta) {this.onItemChange(index, delta)}.bind(this)}
+						path={item.path}
+						item={item.name}
+						price={item.price}
+						numberOfItems={item.numberOfItems}
+						key={item.id} 
+						/>
+						</div>
+						)
+				}.bind(this))}
+			</div>
+			<div className="backgound-2">
+
+			</div>
+			<footer>Thanks for buying with me</footer>
+			
+			</div>
+			);
+		}
+	});
+	ReactDOM.render(<Application initialItems={ITEMS} />, document.getElementById('wrapper'));
+
